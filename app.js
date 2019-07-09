bee35082d7091261cf2b7bd92256d2a2a9276a99
Wyrafinowany <zajac_dominik@onet.pl>
2019-07-09 15:14:26 +0200

    Add path /get

--- app.js
+++ app.js
@@ -16,11 +16,32 @@ const questions = [{
     answers: ['C++', 'Fortran', 'JavaScript', 'Java'],
     correctAnswer: 2,
 }, {
-    question: 'Jaki jest najlepszy język programowania?',
-    answers: ['C++', 'Fortran', 'JavaScript', 'Java'],
-    correctAnswer: 2,
+    question: 'JCzy programowanie jest ciekawe?',
+    answers: ['Nie wiem', 'Oczywiście, że tak', 'Nie', 'Tak'],
+    correctAnswer: 3,
 }, {
-    question: 'Jaki jest najlepszy język programowania?',
-    answers: ['C++', 'Fortran', 'JavaScript', 'Java'],
-    correctAnswer: 2,
-}]
+    question: 'Chcesz zjeść pizzę?',
+    answers: ['Nawet dwie!', 'Jestem na diecie', 'Nie, dziękuję', 'Wolę brokuły'],
+    correctAnswer: 0,
+}];
+
+app.get('question', (req, res) => {
+
+    if (goodAnswers === questions.length) {
+        res.json({
+            winner: true,
+        });
+    } else {
+        const nextQuestion = questions[goodAnswers];
+
+        const {
+            question,
+            answers
+        } = nextQuestion;
+
+        res.json({
+            question,
+            answers,
+        });
+    }
+})
