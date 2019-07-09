dd0ef21e0a6dc6e871eb43c95d6574ab5e5d7cef
Wyrafinowany <zajac_dominik@onet.pl>
2019-07-09 15:04:32 +0200

    Add funny question

--- app.js
+++ app.js
@@ -5,3 +5,22 @@ const app = express();
 app.listen(3000, () => {
     console.log('Servers is listening at http://localhost:3000/');
 });
+
+let goodAnswers = 0;
+let callToAFriendUsed = false;
+let questionToTheCrowdUsed = false;
+let halfOnHalfUsed = false;
+
+const questions = [{
+    question: 'Jaki jest najlepszy język programowania?',
+    answers: ['C++', 'Fortran', 'JavaScript', 'Java'],
+    correctAnswer: 2,
+}, {
+    question: 'Jaki jest najlepszy język programowania?',
+    answers: ['C++', 'Fortran', 'JavaScript', 'Java'],
+    correctAnswer: 2,
+}, {
+    question: 'Jaki jest najlepszy język programowania?',
+    answers: ['C++', 'Fortran', 'JavaScript', 'Java'],
+    correctAnswer: 2,
+}]
